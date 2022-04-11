2819a58e7e3e7bdb92833c0c3ab6e920fda49a04
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-04-11 13:22:06 -0700

    Update trim.cpp

--- code-snippets/formatting/trim.cpp
+++ code-snippets/formatting/trim.cpp
@@ -18,3 +18,29 @@ string trim(string str)
 {
     return ltrim(move(rtrim(str)));
 }
+
+
+
+
+//Templated versions
+template <typename StrT = string, typename CharT = char, typename enable = typename enable_if<is_base_of<basic_string<CharT>,StrT>::value>::type>
+StrT ltrim(StrT str)
+{
+    auto pos = find_if(str.cbegin(), str.cend(), [](char c){return isspace(c) == 0;});
+    typename StrT::size_type idx = distance(str.cbegin(), pos);
+    return str.substr(idx, string::npos);
+}
+
+template <typename StrT = string, typename CharT = char, typename enable = typename enable_if<is_base_of<basic_string<CharT>,StrT>::value>::type>
+StrT rtrim(StrT str)
+{
+    auto pos = find_if(str.rbegin(), str.rend(), [](char c){return isspace(c) == 0;});
+    typename StrT::size_type idx = distance(str.begin(), pos.base());
+    return str.substr(0, idx);
+}
+
+template <typename StrT = string, typename CharT = char, typename enable = typename enable_if<is_base_of<basic_string<CharT>,StrT>::value>::type>
+StrT trim(StrT str)
+{
+    return ltrim(move(rtrim(str)));
+}
