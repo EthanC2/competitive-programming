fea418bcc4de2066dc075e1b933ea2ab6fb6fe27
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-04-11 13:36:07 -0700

    Update sum-digits.cpp

--- code-snippets/math/sum-digits.cpp
+++ code-snippets/math/sum-digits.cpp
@@ -1,1 +1,24 @@
+unsigned int sum_digits(int num)
+{
+    num = abs(num);
+    unsigned int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
 
+template <typename IntT, typename enable = typename enable_if<is_integral<IntT>::value>::type>
+unsigned int sum_digits(IntT num)
+{
+    num = abs(num);
+    unsigned int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
