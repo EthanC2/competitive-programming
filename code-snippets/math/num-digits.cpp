f1a034c520a7f512178ce1d7e76ac708e3211d78
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-04-11 13:30:41 -0700

    Update num-digits.cpp

--- code-snippets/math/num-digits.cpp
+++ code-snippets/math/num-digits.cpp
@@ -1,1 +1,10 @@
+unsigned int num_digits(int num)
+{
+    return (num == 0) ? 1 : log10(abs(num))+1;
+}
 
+template <typename IntT, typename enable = typename enable_if<is_integral<IntT>::value>::type>
+unsigned int num_digits(IntT num)
+{
+    return (num == 0) ? 1 : log10(abs(num))+1;
+}
