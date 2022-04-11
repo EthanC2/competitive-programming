b21143c66b0fa24049389cc7e54f242f00fb79c5
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-04-11 12:42:07 -0700

    Update trim.cpp

--- code-snippets/formatting/trim.cpp
+++ code-snippets/formatting/trim.cpp
@@ -1,14 +1,20 @@
+//Note: since 'isspace' is hella old, it can't be used as a unary predicate
+
 string ltrim(string str)
 {
-
+    auto pos = find_if(str.cbegin(), str.cend(), [](char c){return isspace(c) == 0;});
+    string::size_type idx = distance(str.cbegin(), pos);
+    return str.substr(idx, string::npos);
 }
 
 string rtrim(string str)
 {
-
+    auto pos = find_if(str.rbegin(), str.rend(), [](char c){return isspace(c) == 0;}); 
+    string::size_type idx = distance(str.begin(), pos.base());
+    return str.substr(0, idx);
 }
 
 string trim(string str)
 {
-  return ltrim(rtim(str));
+    return ltrim(move(rtrim(str)));
 }
